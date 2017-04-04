d5adb2d414e6db62fe7cbcdeb84ec719f7a9ec91
Moh <mohakemal9@gmail.com>
2017-04-04 19:41:27 -0400

    updated with comment
    comment was added to the original file as per comment considered.

--- write.js
+++ write.js
@@ -1,5 +1,7 @@
-
+//includes the FS package for writing the package
 var fs = require('fs');
+//running the write file module inside of fs
+//store the write information into the variable data
 fs.writeFile('movie.txt', 'John Wick, cinderela, Titanic', function(error) {
 	if (error){
 	return console.error(error);
